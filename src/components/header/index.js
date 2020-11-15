46b61056060c404075d7d6b0a8d20e7b07a2a09d
leahzheng <zan272905918@163.com>
2020-11-15 08:41:00 +0800

    project start

---
+++ src/components/header/index.js
@@ -0,0 +1,8 @@
+import tpl from './index.tpl';
+import './index.scss';
+
+class Header {
+    
+}
+
+export { Header };
---
+++ src/utils/tools.js
@@ -0,0 +1,42 @@
+function tplReplace(tpl,replaceObject){
+    return tpl.replace(/{{(.*?)}}/g,(node,key)=>{
+        return replaceObject[key]
+    })
+}
+
+function trimSpace(str){
+    return str.replace(/\s+/g,'')
+}
+
+function getUrlQueryValue(key){
+    const reg = RegExp('(^|&)'+key+'=([^&]*)(&|$)','i'),
+          res = window.location.search.substr(1).match(reg);
+    return res != null ? decodeURIComponent(res[2]) : null;
+}
+
+function throttle(fn, delay){
+    var t = null,
+        begin = new Date().getTime();
+    
+    return function(){
+        var _self = this,
+            args = arguments,
+            cur = new Date().getTime();
+
+        clearTimeout(t);
+        if(cur - begin >= delay){
+            fn.apply(_self,args);
+            begin = cur;
+        }else{
+            t = setTimeout(()=>{
+                fn.apply(_self, args);
+            },delay);
+        }
+    }
+}
+module.exports = {
+    tplReplace,
+    trimSpace,
+    getUrlQueryValue,
+    throttle
+}
